9df8f6ff1b7a9f3414574067a0986cbebdc51f0d
konum <darkkonum@gmail.com>
2016-02-18 12:49:15 -0300

    Remove Stream for 1.7

--- Backend/src/main/java/org/wrex/service/impl/PostServiceImpl.java
+++ Backend/src/main/java/org/wrex/service/impl/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package org.wrex.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +26,6 @@ public class PostServiceImpl implements PostService{
 	}
 	@Override
 	public List<Post> getAllPost() {
-		return postDao.getAll().stream().collect(Collectors.toList());
+		return new ArrayList<Post>(postDao.getAll());
 	}
 }
